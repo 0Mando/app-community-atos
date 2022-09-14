d9b91d594225afe36fce2282e98f7be7bf5578f8
Jonathan Javier Oliva Dominguez (ext) <jonathan.oliva.external@atos.net>
2022-09-14 14:42:10 -0700

    Header Integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './presentation/components/landing-page/landing-page.component';
+import { HeaderComponent } from './presentation/components/header/header.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-  		LandingPageComponent
+  		LandingPageComponent,
+    HeaderComponent
 	],
 	imports: [
 		BrowserModule,
---
+++ src/app/presentation/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu():void {
+    let menu  = document.querySelector('.header')!;
+    menu.classList.toggle('active-menu');
+  }
+}
