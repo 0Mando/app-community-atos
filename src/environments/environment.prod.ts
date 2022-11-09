9367293eb3097590b4b7caf61dc31694a112c9a5
OlivaDJohnATOS <109158038+OlivaDJohnATOS@users.noreply.github.com>
2022-11-09 08:35:46 -0800

    Credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,11 +9,12 @@ export const environment = {
   // },
 	production: true,
   firebase: {
-    apiKey: "AIzaSyCVDKeGCwKd_O4CtdZp-BOS-5us066Qtyo",
-    authDomain: "dummy-profile-76979.firebaseapp.com",
-    projectId: "dummy-profile-76979",
-    storageBucket: "dummy-profile-76979.appspot.com",
-    messagingSenderId: "439593482012",
-    appId: "1:439593482012:web:4d7552b2c041e7e01c6cd9"
+    apiKey: "AIzaSyAvXBjkILL17sNq9ys8S1X51Nkld4AsX3A",
+		authDomain: "atos-community-upgrade.firebaseapp.com",
+		databaseURL: "https://atos-community-upgrade-default-rtdb.firebaseio.com",
+		projectId: "atos-community-upgrade",
+		storageBucket: "atos-community-upgrade.appspot.com",
+		messagingSenderId: "806304666512",
+		appId: "1:806304666512:web:78203b44c10f6ec87c9915"
   }
 };
