7803da08b7e0a5905decbd68b5411ea481185fd5
Luis Leal <luis-angel.lealhernandez.external@atos.net>
2022-11-11 10:45:10 -0600

    Button actions in comments

--- src/app/community/components/comments/button-actions/button-actions.component.ts
+++ src/app/community/components/comments/button-actions/button-actions.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-button-actions',
-  templateUrl: './button-actions.component.html',
-  styleUrls: ['./button-actions.component.scss']
+	selector: 'app-button-actions',
+	templateUrl: './button-actions.component.html',
+	styleUrls: ['./button-actions.component.scss']
 })
 export class ButtonActionsComponent implements OnInit {
 
-  constructor() { }
+	showOptionsList : boolean = false;
+	commentAuthorId : string = '';
+	@Input() canEdit : boolean;
+	@Input() canDelete : boolean;
+	@Input() canReport : boolean;
 
-  ngOnInit(): void {
-  }
+	@Output() editComment = new EventEmitter<boolean>();
+	@Output() deleteComment = new EventEmitter<boolean>();
+	@Output() reportComment = new EventEmitter<boolean>();
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	onPressedOptions() : void {
+		this.showOptionsList = !this.showOptionsList;
+	}
+
+	onPressedCover () : void {
+		this.showOptionsList = !this.showOptionsList;
+	}
+
+	onEdit() : void {
+		this.editComment.emit(true);
+	}
+
+	onDelete() : void {
+		alert('Deleting');
+	}
+
+	onReport() : void {
+		alert('Reporting');
+	}
 
 }
