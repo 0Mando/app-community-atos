15e5d46f1922ecc99c680ab990292760e561ec6e
OlivaDJohnATOS <109158038+OlivaDJohnATOS@users.noreply.github.com>
2022-11-25 07:39:15 -0800

    User NAME

--- src/app/domain/models/user.model.ts
+++ src/app/domain/models/user.model.ts
@@ -1,7 +1,8 @@
 export interface User{
 	id? : string;
-	firstName: string;
-	lastName: string;
+	firstName?: string;
+	lastName?: string;
+	name?: string;
 	birthday: string;
 	email: string;
 	password: string;
