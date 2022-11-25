3cbfac3b99df549f031e4ce466a6087a87c64cd9
Luis Leal <luis-angel.lealhernandez.external@atos.net>
2022-11-25 09:14:56 -0600

    Delete old submit post method

--- src/app/community/components/article/pages/create-article/create-article.component.ts
+++ src/app/community/components/article/pages/create-article/create-article.component.ts
@@ -123,49 +123,6 @@ export class CreateArticleComponent implements OnInit, ArticleCanDeactivate {
 		this.sendArticle(this.post);
 	}
 
-	// TODO : Issue submit information, save draft
-	submitPost(): void {
-		this.post = {
-			userCreatedId: this.authenticationService.currentSessionUserId(),
-			date: this.currentDate.getTime(),
-			channelId: this.channelIdParam,
-			titlePost: this.markdownForm.get('titlePostForm').value || '',
-			descriptionContent: this.markdownForm.get('descriptionContentForm').value || '',
-			content: this.markdownForm.get('contentForm').value || '',
-			disableComments: this.markdownForm.get('comments').value || '',
-			archive: this.archiveArticle,
-			readingTime: this.markdownForm.get('readingTimeForm').value || 0,
-		}
-		console.table(this.post);
-
-		if (this.markdownForm.valid) {
-			// *Send post to database
-			this.articleService.createPost(this.post).catch(
-				error => console.log('An error ocurred : ' + error)
-			)
-			// *Reset form
-			// this.markdownForm.reset();
-			// *Go back to list articles page
-			this.router.navigate(['/articles/' + this.channelIdParam + '/posts']);
-			// *Changes saved
-			this.articleChangesSaved = true;
-		} else if (!this.markdownForm.valid && this.post.archive) {
-			// *Send post to database
-			this.articleService.createPost(this.post).catch(
-				error => console.log('An error ocurred : ' + error)
-			)
-			// *Reset form
-			// this.markdownForm.reset();
-			// *Go back to list articles page
-			this.router.navigate(['/articles/' + this.channelIdParam + '/posts']);
-			// *Changes saved
-			this.articleChangesSaved = true;
-		} else {
-			alert('Complete form or save your draft')
-		}
-
-	}
-
 	/**
 	 * Archive the current article.
 	 */
