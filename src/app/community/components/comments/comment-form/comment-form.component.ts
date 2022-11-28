052059792f0a0ae773934b4c89ff4e09b91eb650
Luis Leal <luis-angel.lealhernandez.external@atos.net>
2022-11-28 12:49:43 -0600

    Settings quill editor comment form

--- src/app/community/components/comments/comment-form/comment-form.component.ts
+++ src/app/community/components/comments/comment-form/comment-form.component.ts
@@ -27,48 +27,6 @@ export class CommentFormComponent implements OnInit {
 
 	private idUser: string = this.authenticationService.currentSessionUserId();
 
-	//* Toolbar settings input text for create a post
-	editorModules = {
-		toolbar: [
-			[
-				// 'bold',
-				// 'italic',
-				// 'underline',
-				// 'strike'
-			],
-			[
-				// 'blockquote',
-				'code-block'
-			],
-			// [{ 'header' : 1 }, { 'header' : 2 }],
-			// [{ 'list' : 'ordered' }, { 'list' : 'bullet' }],
-			// [{ 'script' : 'sub' }, { 'script' : 'super' }],
-			// [{ 'indent' : '-1' }, { 'indent' : '+1' }],
-			// [{ 'direction' : 'rtl' }],
-
-			//* Toolbar fontsize stuff
-			// [{ 'size' : ['small', false, 'large', 'huge'] }],
-			// [{ 'header' : [1,2,3,4,5,6, false] }],
-
-			//* Toolbar font stuff
-			[
-				// { 'color' : [] },
-				// { 'background' : [] }
-			],
-			// [{ 'font' : [] }],
-			// [{ 'align' : ['', 'center', 'right', 'justify'] }],
-
-			// ['clean'],
-
-			//* Toolbar multimedia
-			[
-				// 'link',
-				// 'image',
-				// 'video'
-			]
-		]
-	}
-
 	constructor(
 		private commentsService: CommentsService,
 		private authenticationService: AuthService,
@@ -120,5 +78,47 @@ export class CommentFormComponent implements OnInit {
 				}
 			);
 		this.commentForm.reset();
+	}
+
+	//* Toolbar settings input text for create a post
+	editorModules = {
+		toolbar: [
+			[
+				// 'bold',
+				// 'italic',
+				// 'underline',
+				// 'strike'
+			],
+			[
+				// 'blockquote',
+				'code-block'
+			],
+			// [{ 'header' : 1 }, { 'header' : 2 }],
+			// [{ 'list' : 'ordered' }, { 'list' : 'bullet' }],
+			// [{ 'script' : 'sub' }, { 'script' : 'super' }],
+			// [{ 'indent' : '-1' }, { 'indent' : '+1' }],
+			// [{ 'direction' : 'rtl' }],
+
+			//* Toolbar fontsize stuff
+			// [{ 'size' : ['small', false, 'large', 'huge'] }],
+			// [{ 'header' : [1,2,3,4,5,6, false] }],
+
+			//* Toolbar font stuff
+			[
+				// { 'color' : [] },
+				// { 'background' : [] }
+			],
+			// [{ 'font' : [] }],
+			// [{ 'align' : ['', 'center', 'right', 'justify'] }],
+
+			// ['clean'],
+
+			//* Toolbar multimedia
+			[
+				// 'link',
+				// 'image',
+				// 'video'
+			]
+		]
 	}
 }
