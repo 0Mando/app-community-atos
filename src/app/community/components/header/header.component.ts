1b6d9bc662e485db7af9b9831efb9c136b66544f
OlivaDJohnATOS <109158038+OlivaDJohnATOS@users.noreply.github.com>
2022-11-29 19:54:55 -0800

    Header Profile Bug Fix

--- src/app/community/components/header/header.component.ts
+++ src/app/community/components/header/header.component.ts
@@ -14,6 +14,7 @@ import { BoardService } from 'src/app/infrastructure/services/board.service';
 export class HeaderComponent implements OnInit {
 
 	currentUser: User;
+	itExists: boolean;
 
 	boards: Board[];
 	profiles: string[] = [
@@ -61,6 +62,8 @@ export class HeaderComponent implements OnInit {
 					this.currentUser = {
 						...data.payload.data()
 					}
+					this.itExists = data.payload.exists
+					
 				})
 			))
 		).subscribe();
